7cf4a475ac88dafecb6bd7f7d27749dc7592cf43
pplsk2524 <ppoojitha4252@gmail.com>
2026-08-08 13:11:39 +0530

    refactor: added exception for validations

--- product-service/src/main/java/com/poojitha/product_service/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/poojitha/product_service/exception/GlobalExceptionHandler.java
@@ -2,14 +2,26 @@ package com.poojitha.product_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
